fd15b3b311ee33d47745a983578db19735658aca
Seorin <seorinpark96@gmail.com>
2022-01-02 17:50:38 +0900

    feat : Atomic design기반 boilerplate 작성

---
+++ client/src/components/atoms/Card/index.js
@@ -0,0 +1,4 @@
+// 칼로리 및 영양소 상세정보, 음식 상세정보 카드, 담긴 음식카드, 산책로카드에 사용
+// styled component로 변경해야지
+// 아래 코드는 typescript라 나중에 변경해야됨
+// https://github.com/danilowoz/react-atomic-design/blob/master/src/components/atoms/card/index.js
---
+++ client/src/components/molecules/InfoConfirmModal/index.js
@@ -0,0 +1,2 @@
+// 사용자 정보 확인 모달
+// Modal, Title, Button 사용 
